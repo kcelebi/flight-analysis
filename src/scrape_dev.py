6eabb1d04ad20f7735b5a92008d3c9a95ddfed8c
Kaya Celebi <kayacelebi17@gmail.com>
2023-04-04 21:15:37 -0400

    Adding functionalities to new scrape dev, testing circleci with chromedriver

--- src/scrape_dev.py
+++ src/scrape_dev.py
@@ -13,10 +13,7 @@ from tqdm import tqdm
 import json
 import os
 
-__all__ = ['CacheControl', 'Scrape']
-
-class _CacheControl:
-	...
+__all__ = ['Scrape']
 
 class _Scrape:
 
@@ -26,10 +23,12 @@ class _Scrape:
 		self._date_leave = None
 		self._date_return = None
 		self._cache = None
+		self._data = None
 
 	def __call__(self, *args):
 		self._set_properties(*args)
-		self._scrape_data()
+		self._data = self._scrape_data()
+		return self
 
 	def __str__(self):
 		return "{dl}: {org} --> {dest}\n{dr}: {dest} --> {org}".format(
@@ -47,13 +46,17 @@ class _Scrape:
 			dest = self._dest
 		)
 
+	def clone(self):
+		return _Scrape()._set_properties(
+			self._origin, self._dest, self._date_leave, self._date_return
+		)
+
 	'''
 		Set properties upon scraper called.
 	'''
 	def _set_properties(self, *args):
 		(
-			self._origin, self._dest, self._date_leave,
-			self._date_return, self._cache
+			self._origin, self._dest, self._date_leave, self._date_return
 		) = args
 
 	@property
@@ -88,6 +91,14 @@ class _Scrape:
 	def date_return(self, x : str) -> None:
 		self._date_return = x
 
+	@property
+	def data(self):
+		return self._data
+
+	@data.setter
+	def data(self, x):
+		self._data = x
+
 	'''
 		Scrape the object
 	'''
@@ -108,13 +119,13 @@ class _Scrape:
 		results = _Scrape._make_url_request(url)
 
 		flight_info = _Scrape._get_info(results)
-		parition = _Scrape._partition_info(flight_info)
+		partition = _Scrape._partition_info(flight_info)
 
-		return parse_columns
+		return _Scrape._parse_columns(partition, self._date_leave, self._date_return)
 
 	@staticmethod
 	def _make_url_request(url):
-		driver = webdriver.Chrome('/Users/kayacelebi/Downloads/chromedriver')
+		driver = webdriver.Chrome()#'/Users/kayacelebi/Downloads/chromedriver')
 		driver.get(url)
 
 		# Waiting and initial XPATH cleaning
@@ -122,6 +133,8 @@ class _Scrape:
 		results = _Scrape._get_flight_elements(driver)
 
 		driver.quit()
+
+		return results
 
 	@staticmethod
 	def _get_flight_elements(driver):
@@ -145,8 +158,112 @@ class _Scrape:
 
 	@staticmethod
 	def _partition_info(info):
-		...
+		i, grouped = 0, []
+		while i < len(info) - 1:
+			j = i + 2
+			end = -1
+			while j < len(info):
+				if _Scrape._end_condition(info[j]):
+					end = j
+					break
+				j += 1
+
+			if end == -1:
+				break
+			grouped += [info[i:end]]
+			i = end
+
+		return grouped
+
+	@staticmethod
+	def _end_condition(x):
+		if len(x) < 2:
+			return False
+
+		if x[-2] == "+":
+			x = x[:-2]
+
+		if x[-2:] == 'AM' or x[-2:] == 'PM':
+			return True
+		return False
+
+	@staticmethod
+	def _parse_columns(grouped, date_leave, date_return):
+		# Instantiate empty column arrays
+		depart_time = []
+		arrival_time = []
+		airline = []
+		travel_time = []
+		origin = []
+		dest = []
+		stops = []
+		stop_time = []
+		stop_location = []
+		co2_emission = []
+		emission = []
+		price = []
+		trip_type = []
+		access_date = [date.today().strftime('%Y-%m-%d')]*len(grouped)
+
+		# For each "flight"
+		for g in grouped:
+			i_diff = 0 # int that checks if we need to jump ahead based on some conditions
+
+			# Get departure and arrival times
+			depart_time += [g[0]]
+			arrival_time += [g[1]]
+
+			# When this string shows up we jump ahead an index
+			i_diff += 1 if 'Separate tickets booked together' in g[2] else 0
+
+			# Add airline, travel time, origin, and dest
+			airline += [g[2 + i_diff]]
+			travel_time += [g[3 + i_diff]]
+			origin += [g[4 + i_diff].split('–')[0]]
+			dest += [g[4 + i_diff].split('–')[1]]
+
+			# Grab the number of stops by splitting string
+			num_stops = 0 if 'Nonstop' in g[5 + i_diff] else int(g[5 + i_diff].split('stop')[0])
+			stops += [num_stops]
+
+			# Add stop time/location given whether its nonstop flight or not
+			stop_time += [None if num_stops == 0 else (g[6 + i_diff].split('min')[0] if num_stops == 1 else None)]
+			stop_location += [None if num_stops == 0 else (g[6 + i_diff].split('min')[1] if num_stops == 1 and 'min' in g[6 + i_diff] else [g[6 + i_diff].split('hr')[1] if 'hr' in g[6 + i_diff] and num_stops == 1 else g[6 + i_diff]])]
+
+			# Jump ahead an index if flight isn't nonstop to accomodate for stop_time, stop_location
+			i_diff += 0 if num_stops == 0 else 1
+
+			# If Co2 emission not listed then we skip, else we add
+			if g[6 + i_diff] != '–':
+				co2_emission += [float(g[6 + i_diff].replace(',','').split(' kg')[0])]
+				emission += [0 if g[7 + i_diff] == 'Avg emissions' else int(g[7 + i_diff].split('%')[0])]
+
+				price += [float(g[8 + i_diff][1:].replace(',',''))]
+				trip_type += [g[9 + i_diff]]
+			else:
+				co2_emission += [None]
+				emission += [None]
+				price += [float(g[7 + i_diff][1:].replace(',',''))]
+				trip_type += [g[8 + i_diff]]
 
 
-CacheControl = _CacheControl()
+		return pd.DataFrame({
+			'Leave Date' : [date_leave]*len(grouped),
+			'Return Date' : [date_return]*len(grouped),
+			'Depart Time (Leg 1)' : depart_time,
+			'Arrival Time (Leg 1)' : arrival_time,
+			'Airline(s)' : airline,
+			'Travel Time' : travel_time,
+			'Origin' : origin,
+			'Destination' : dest,
+			'Num Stops' : stops,
+			'Layover Time' : stop_time,
+			'Stop Location' : stop_location,
+			'CO2 Emission' : co2_emission,
+			'Emission Avg Diff (%)' : emission,
+			'Price ($)' : price,
+			'Trip Type' : trip_type,
+			'Access Date' : access_date
+		})
+
 Scrape = _Scrape()
--- tests/test_class.py
+++ tests/test_class.py
@@ -1,9 +1,30 @@
 import pytest
 from pathlib import Path
-from src.scraping import *
+#from src.scraping import *
+from src.scrape_dev import *
 
 def func_0():
 	return True
 
+def func_1():
+	return Scrape("LGA", "RDU", "2023-05-15", "2023-06-15")
+
+func1res = func_1()
+
 def test_0():
-	assert func_0, "Test 0 Failed"
+	assert func_0(), "Test 0 Failed"
+
+def test_1():
+	assert func1res.data.shape[0] > 0, "Test 1 Failed."
+
+def test_2():
+	assert func1res.origin == "LGA", "Test 2 Failed."
+
+def test_3():
+	assert func1res.dest == "RDU", "Test 3 Failed."
+
+def test_4():
+	assert func1res.date_leave == "2023-05-15", "Test 4 Failed."
+
+def test_5():
+	assert func1res.date_return == "2023-06-15", "Test 5 Failed."
